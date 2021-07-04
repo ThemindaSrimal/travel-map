f11ac1dfcd1f43e52a881c2e40c70da539e35318
themindasrimal <themindasrimal@gmail.com>
2021-07-04 09:42:11 +0530

    register from

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,14 +1,20 @@
 import "./register.css";
+import {Room} from "@material-ui/icons";
 
 export default function Register(){
     return (
         <div className="registerContainer">
-            <div className="logo"></div>
+            <div className="logo">
+                <Room/>
+                TS Pin
+            </div>
             <form>
                 <input type="text" placeholder="username"/>
                 <input type="email" placeholder="email"/>
                 <input type="password" placeholder="password"/>
-                <button>Register</button>
+                <button className="registerBtn">Register</button>
+                <span className="success">Successfull. You can login now!</span>
+                <span className="failure">Something went wrong!</span>
             </form>
         </div>
     )
